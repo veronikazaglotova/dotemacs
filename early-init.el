c4bb418014aa0e908d773755e4b8dd1bd4568b2e
veronikazaglotova <zelenaruta@gmail.com>
2021-02-14 23:39:57 +0600

    added early-init.el

---
+++ early-init.el
@@ -0,0 +1,18 @@
+(setq straight-use-package-by-default t)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+(setq use-package-always-demand t)
+(setq custom-file (concat user-emacs-directory "customize.el"))
+(when (file-exists-p custom-file)
+  (load-file custom-file))
